7f5049c1e1dcd144c25ff7044549785d57bfb5db
tsunami493 <40723850+tsunami493@users.noreply.github.com>
2018-12-18 10:52:59 -0500

    Add files via upload

---
+++ PokerGame/PokerGame/Form1.Designer.vb
@@ -0,0 +1,378 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnNewDeal = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCall = New System.Windows.Forms.Button()
+        Me.btnRaise = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.lblpotTotal = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnNewDeal
+        '
+        Me.btnNewDeal.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnNewDeal.Location = New System.Drawing.Point(12, 561)
+        Me.btnNewDeal.Name = "btnNewDeal"
+        Me.btnNewDeal.Size = New System.Drawing.Size(116, 63)
+        Me.btnNewDeal.TabIndex = 0
+        Me.btnNewDeal.Text = "New Deal"
+        Me.btnNewDeal.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(362, 727)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(519, 727)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'btnCall
+        '
+        Me.btnCall.Location = New System.Drawing.Point(884, 692)
+        Me.btnCall.Name = "btnCall"
+        Me.btnCall.Size = New System.Drawing.Size(112, 51)
+        Me.btnCall.TabIndex = 3
+        Me.btnCall.Text = "Call"
+        Me.btnCall.UseVisualStyleBackColor = True
+        '
+        'btnRaise
+        '
+        Me.btnRaise.Location = New System.Drawing.Point(1123, 693)
+        Me.btnRaise.Name = "btnRaise"
+        Me.btnRaise.Size = New System.Drawing.Size(114, 50)
+        Me.btnRaise.TabIndex = 4
+        Me.btnRaise.Text = "Raise"
+        Me.btnRaise.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1012, 693)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(105, 50)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Check"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(1243, 693)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(102, 50)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Fold"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(235, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(477, 175)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Label4"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(379, 231)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 20)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Flop"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(648, 231)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(41, 20)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Turn"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(799, 231)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(45, 20)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "River"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(767, 650)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 13
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(767, 692)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(111, 51)
+        Me.Button6.TabIndex = 14
+        Me.Button6.Text = "Bet"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(1123, 661)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox2.TabIndex = 15
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(1081, 491)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(37, 20)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "Pot:"
+        '
+        'lblpotTotal
+        '
+        Me.lblpotTotal.AutoSize = True
+        Me.lblpotTotal.Location = New System.Drawing.Point(1117, 491)
+        Me.lblpotTotal.Name = "lblpotTotal"
+        Me.lblpotTotal.Size = New System.Drawing.Size(13, 20)
+        Me.lblpotTotal.TabIndex = 17
+        Me.lblpotTotal.Text = " "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(684, 507)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 20)
+        Me.Label5.TabIndex = 30
+        Me.Label5.Text = " "
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox3.Location = New System.Drawing.Point(123, 283)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox3.TabIndex = 29
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox2.Location = New System.Drawing.Point(491, 520)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox2.TabIndex = 28
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox1.Location = New System.Drawing.Point(333, 520)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox1.TabIndex = 27
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox9.Location = New System.Drawing.Point(466, 12)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox9.TabIndex = 26
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox8.Location = New System.Drawing.Point(308, 12)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox8.TabIndex = 25
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox7.Location = New System.Drawing.Point(765, 283)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox7.TabIndex = 24
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox6.Location = New System.Drawing.Point(603, 304)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(86, 124)
+        Me.PictureBox6.TabIndex = 23
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox5.Location = New System.Drawing.Point(422, 283)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox5.TabIndex = 22
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.WindowsApp1.My.Resources.Resources.CardBack
+        Me.PictureBox4.Location = New System.Drawing.Point(274, 283)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(97, 145)
+        Me.PictureBox4.TabIndex = 21
+        Me.PictureBox4.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1489, 863)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.lblpotTotal)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btnRaise)
+        Me.Controls.Add(Me.btnCall)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnNewDeal)
+        Me.Name = "Form1"
+        Me.Text = "Poker Game"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnNewDeal As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCall As Button
+    Friend WithEvents btnRaise As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents lblpotTotal As Label
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ PokerGame/PokerGame/Form1.vb
@@ -0,0 +1,347 @@
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
+    Dim pot As Integer = 0
+    Dim strPath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+    Dim i As Integer = strPath.Count
+    Dim strpath2 As String = strPath.Substring(6, i - 16)
+    Dim path As String = strpath2 + "\Resources\"
+    Dim blk() As PictureBox
+    Dim win As Boolean
+    Dim turn As Boolean
+    Enum Face
+        Ace
+        Two
+        Three
+        Four
+        Five
+        Six
+        Seven
+        Eight
+        Nine
+        Ten
+        Jack
+        Queen
+        King
+    End Enum
+    Enum Suit
+        Spades
+        Hearts
+        Clubs
+        Diamonds
+    End Enum
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        blk = New PictureBox() {PictureBox1, PictureBox2, PictureBox3, PictureBox4, PictureBox5, PictureBox6, PictureBox7, PictureBox8, PictureBox9}
+        Console.WriteLine(blk)
+        Dim j As Integer = 1
+        For i = 2 To 6
+            blk(i).Top = 167
+            blk(i).Left = 100 * j
+            j += 1
+        Next
+
+        For i = 0 To 8
+            blk(i).Width = 82
+            blk(i).Height = 130
+            blk(i).SizeMode = PictureBoxSizeMode.StretchImage
+            Me.Controls.Add(blk(i))
+        Next
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnNewDeal.Click
+        Randomize()
+        Dim cardpack As New ArrayList
+        Dim Suits = [Enum].GetNames(GetType(Suit))
+        Dim Faces = [Enum].GetNames(GetType(Face))
+        For Each Face In Faces 'Initizalizing cardpack with 52 cards
+            For Each Suit In Suits
+                cardpack.Add(Face + Suit)
+            Next
+        Next
+        Dim ShuffledDeck As New ArrayList
+        Dim rand As New Random
+        While cardpack.Count > 0 'Adding cards at random to new arraylist and ensuring no double picks
+            Dim index As Integer = rand.Next(0, cardpack.Count)
+            ShuffledDeck.Add(cardpack(index))
+            cardpack.RemoveAt(index)
+        End While
+
+        Dim playerHand As New ArrayList
+        For i = 0 To 1 'Taking cards from top of the deck, from index 0 and removing them
+            playerHand.Add(ShuffledDeck(0))
+            ShuffledDeck.RemoveAt(0)
+        Next
+        Dim playerPlayedCards As New ArrayList
+        For i = 0 To 1
+            blk(i).Image = Image.FromFile(path + playerHand(i) + ".png")
+            playerPlayedCards.Add(playerHand(i))
+        Next
+        Label1.Text = playerHand(0)
+        Label2.Text = playerHand(1)
+        Dim computerPlayedCards As New ArrayList
+        Dim computerHand As New ArrayList
+        For i = 0 To 1
+            computerHand.Add(ShuffledDeck(0))
+            ShuffledDeck.RemoveAt(0)
+            computerPlayedCards.Add(computerHand)
+        Next
+        Label3.Text = computerHand(0)
+        Label4.Text = computerHand(1)
+
+        Dim communityCards As New ArrayList
+        For i = 0 To 4
+            communityCards.Add(ShuffledDeck(0))
+            ShuffledDeck.RemoveAt(0)
+        Next
+
+        For i = 0 To 4
+            blk(i + 2).Image = Image.FromFile(path + communityCards(i) + ".png")
+            playerPlayedCards.Add(communityCards(i))
+            computerPlayedCards.Add(communityCards(i))
+        Next
+
+        'Dim playerPlayedCards As String() = {"AceSpades", "AceClubs", "TwoDiamonds", "TwoHearts", "FiveHearts", playerHand(0), playerHand(1)}
+    End Sub
+    'Function IsFlush2(ByVal card As ArrayList) As String
+    '    Dim myDict As New Dictionary(Of String, ArrayList)
+    '    Dim operands As New ArrayList
+    '    Dim return_str As String = ""
+    '    For i = 0 To 6
+    '        operands.Add(Regex.Split(card(i), "(?=[A-Z])"))
+    '    Next
+
+    '    Dim ArrayItem As String
+    '    For i = 0 To 6
+    '        ArrayItem = operands(i)(2)
+    '        If myDict.ContainsKey(ArrayItem) Then
+    '            myDict(ArrayItem) = myDict(ArrayItem + 1)
+    '        Else
+    '            myDict.Add(ArrayItem, operands(i)(2))
+    '        End If
+    '    Next
+    '    Dim keys As List(Of String) = myDict.Keys.ToList
+    '    keys.Sort()
+    '    Dim str As String
+    '    For Each str In keys
+    '        If myDict.Item(str) >= 4 Then
+    '            return_str = str
+    '        End If
+    '    Next
+    '    Return return_str
+    'End Function
+
+    Function IsStraightFlush(ByVal card As ArrayList) As Boolean
+        If IsFlush(card) <> "False" And IsStraight(card) = True Then
+            Return True
+        End If
+        Return False
+    End Function
+    Function FourOfAKind(ByVal card As ArrayList) As Boolean
+        Dim Faces = [Enum].GetNames(GetType(Face))
+        Dim QuadDict As New Dictionary(Of String, Integer)
+        Dim QuadFaces As New ArrayList
+        Dim cardArray As New ArrayList
+        For Each Face In Faces
+            QuadFaces.Add(Face)
+        Next
+        For i = 0 To 6
+            cardArray.Add(Regex.Split(card(i), "(?=[A-Z])"))
+        Next
+        For i = 0 To 12
+            QuadDict.Add(QuadFaces(i), 0)
+        Next
+        For i = 0 To 6
+            QuadDict(cardArray(i)(1)) += 1
+        Next
+        For i = 0 To 12
+            If QuadDict(QuadFaces(i)) > 0 Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+    Function IsFullHouse(ByVal card As ArrayList) As String
+        If IsFullHouse(card) = True Then
+            Dim Faces = [Enum].GetNames(GetType(Face))
+            Dim FullHouseDict As New Dictionary(Of String, Integer)
+            Dim FullHouseFaces As New ArrayList
+            Dim cardArray As New ArrayList
+            For Each Face In Faces
+                FullHouseFaces.Add(Face)
+            Next
+            For i = 0 To 6
+                cardArray.Add(Regex.Split(card(i), "(?=[A-Z])"))
+            Next
+            For i = 0 To 12
+                FullHouseDict.Add(FullHouseFaces(i), 0)
+            Next
+            For i = 0 To 6
+                FullHouseDict(cardArray(i)(1)) += 1
+            Next
+            For i = 0 To 12
+                If FullHouseDict(FullHouseFaces(i)) = 2 Then
+                    Return "Full House"
+                End If
+            Next
+        End If
+        Return "False"
+    End Function
+    Function IsFlush(ByVal card As ArrayList) As String
+        Dim pattern() As String = {"*Diamonds", "*Clubs", "*Hearts", "*Spades"}
+        Dim cardStr As String = ""
+        For i = 0 To 6
+            For j = 0 To 3
+                If card(i) Like pattern(j) = True Then
+                    cardStr = cardStr + CStr(j + 1)
+                End If
+            Next
+        Next
+        Dim suitsArray() As Integer = {0, 0, 0, 0}
+        While cardStr.Length > 0
+            For i = 1 To 4
+                If cardStr.IndexOf(i) <> -1 Then
+                    Dim y As Integer = cardStr.IndexOf(i)
+                    cardStr = cardStr.Remove(y, 1)
+                    suitsArray(i - 1) += 1
+                End If
+            Next
+        End While
+        For i = 0 To 3
+            If suitsArray(i) >= 5 Then
+                If i = 0 Then
+                    Return "Diamonds"
+                ElseIf i = 1 Then
+                    Return "Clubs"
+                ElseIf i = 2 Then
+                    Return "Hearts"
+                ElseIf i = 3 Then
+                    Return "Spades"
+                End If
+            End If
+        Next
+        Return "False"
+    End Function
+    Function IsStraight(ByVal card As ArrayList) As String
+        Dim Faces = [Enum].GetNames(GetType(Face))
+        Dim straightDict As New Dictionary(Of String, Integer)
+        Dim straightFaces As New ArrayList
+        Dim cardArray As New ArrayList
+        Dim return_str As String
+        For Each Face In Faces
+            straightFaces.Add(Face)
+        Next
+        For i = 0 To 6
+            cardArray.Add(Regex.Split(card(i), "(?=[A-Z])"))
+        Next
+        For i = 0 To 12
+            straightDict.Add(straightFaces(i), 0)
+        Next
+        For i = 0 To 6
+            straightDict(cardArray(i)(1)) += 1
+        Next
+        For i = 0 To 8
+            If straightDict(straightFaces(i)) > 0 Then
+                If straightDict(straightFaces(i + 1)) > 0 Then
+                    If straightDict(straightFaces(i + 2)) > 0 Then
+                        If straightDict(straightFaces(i + 3)) > 0 Then
+                            If straightDict(straightFaces(i + 4)) > 0 Then
+                                return_str = straightFaces(i + 4)
+                                Return return_str
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+        Next
+
+        Return "False"
+    End Function
+
+    Function IsTriple(ByVal card As ArrayList) As String
+        Dim Faces = [Enum].GetNames(GetType(Face))
+        Dim TripleDict As New Dictionary(Of String, Integer)
+        Dim TripleFaces As New ArrayList
+        Dim cardArray As New ArrayList
+        For Each Face In Faces
+            TripleFaces.Add(Face)
+        Next
+        For i = 0 To 6
+            cardArray.Add(Regex.Split(card(i), "(?=[A-Z])"))
+        Next
+        For i = 0 To 12
+            TripleDict.Add(TripleFaces(i), 0)
+        Next
+        For i = 0 To 6
+            TripleDict(cardArray(i)(1)) += 1
+        Next
+        For i = 0 To 11
+            If TripleDict(TripleFaces(i)) = 3 Then
+                Return TripleFaces(i)
+            End If
+        Next
+        Return "No Triple"
+    End Function
+
+    Function TwoPairOrSinglePair(ByVal card As ArrayList) As String
+        Dim Faces = [Enum].GetNames(GetType(Face))
+        Dim PairDict As New Dictionary(Of String, Object)
+        Dim PairFaces As New ArrayList
+        Dim cardArray As New ArrayList
+        For Each Face In Faces
+            PairFaces.Add(Face)
+        Next
+        For i = 0 To 6
+            cardArray.Add(Regex.Split(card(i), "(?=[A-Z])"))
+            cardArray(i)(0) = 0
+            If cardArray(i)(2) = "Spades" Then
+                cardArray(i)(2) = CInt(4)
+            ElseIf cardArray(i)(2) = "Hearts" Then
+                cardArray(i)(2) = CInt(3)
+            ElseIf cardArray(i)(2) = "Clubs" Then
+                cardArray(i)(2) = CInt(2)
+            ElseIf cardArray(i)(2) = "Diamonds" Then
+                cardArray(i)(2) = CInt(1)
+            End If
+        Next
+        For i = 0 To 12
+            PairDict.Add(PairFaces(i), {0, 0})
+        Next
+        For i = 0 To 6
+            If PairDict.ContainsKey(cardArray(i)(1)) Then
+                PairDict(cardArray(i)(1))(0) += 1
+                Dim oldSuit As Integer = PairDict(cardArray(i)(1))(1)
+                If cardArray(i)(2) > oldSuit Then
+                    PairDict(cardArray(i)(1))(1) = cardArray(i)(2)
+                Else
+                    PairDict(cardArray(i)(1))(1) = oldSuit
+                End If
+            End If
+        Next
+        Dim pairCount As Integer = 0
+        Dim pairValue As String = ""
+        For i = 0 To 12
+            If PairDict(PairFaces(i))(0) = 2 Then
+                pairCount += 1
+                pairValue = PairFaces(i)
+            End If
+        Next
+        If pairCount = 1 Then
+            Return pairValue
+        ElseIf pairCount > 1 Then
+            Return "Two Pair"
+        End If
+        Return "No Pair"
+    End Function
+
+    'Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+    '    Dim BetAmount As Integer = Val(Me.TextBox1.Text)
+
+    '    If BetAmount = 0 Then
+    '        MsgBox("Enter a bet amount!")
+    '    Else
+
+    '        pot += BetAmount
+    '    End If
+    '    lblpotTotal.Text = pot
+    '    TextBox1.Text = ""
+    'End Sub
+
+End Class
